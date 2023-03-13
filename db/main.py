bc6e36da44065a540a0dbed02dbe7c1c13f17c46
Xpystum <qjq3@mail.ru>
2023-03-13 07:40:14 +0300

    Text db (test)
    Тестовой main файл для теста БД

--- db/main.py
+++ db/main.py
@@ -1,35 +1,33 @@
 import pymysql
-from mysql.connector import connect, Error
+import pprint
+from mysql.connector import connect, errorcode, Error
 from config import host, user, password, db_name
 
 try:
-    connection = pymysql.connect(
-    host = host,
-    port=3306,
+    with connect(
     user=user,
     password= password,
+    host = host,
     database=db_name,
-    cursorclass= pymysql.cursors.DictCursor
-    )
-    print("Successfully connected")
-    print('"#' * 20)
+    ) as connection:
+        print(connection)
+    # print("Successfully connected")
+    # print('"#' * 20)
 
-    # try:
-    #    
-    # except:
-    #     sdfgsdf
-    # finally:
-    #     connection.close()
     try:
          with connection.cursor() as cursor:
              show_table = "show tables"
              cursor.execute(show_table)
-             print(f"Table creares successfully:\t {cursor.fetchall()}")
+             pprint.print(f"Table creares successfully:\t {cursor.fetchall()}")
 
     finally:
         connection.close()
 
-except Exception as ex:
-    print("Connection refused")
-    print(ex)
-
+except Error as err:
+    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        print("Something is wrong with your user name or password")
+    elif err.errno == errorcode.ER_BAD_DB_ERROR:
+        print("Database does not exist")
+    else:
+        print(err)
+        
