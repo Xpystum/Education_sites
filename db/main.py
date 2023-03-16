237192429e01afb6cb7ce9b429336e45c6489952
Xpystum <qjq3@mail.ru>
2023-03-16 10:09:26 +0300

    Site + DataBase MySQL

--- db/main.py
+++ db/main.py
@@ -1,30 +1,38 @@
 import pymysql
-from mysql.connector import connect, Error
+import pprint
 from config import host, user, password, db_name
 
 try:
     connection = pymysql.connect(
     host = host,
-    port=3306,
-    user=user,
+    port= 3306,
+    user= user,
     password= password,
     database=db_name,
-    cursorclass= pymysql.cursors.DictCursor
+    cursorclass= pymysql.cursors.DictCursor # это нужно для того, чтобы получить результат в виде словаря, где ключами будут названия колонок.
     )
     print("Successfully connected")
     print('"#' * 20)
 
-    # try:
-    #    
-    # except:
-    #     sdfgsdf
-    # finally:
-    #     connection.close()
     try:
          with connection.cursor() as cursor:
-             show_table = "show tables"
-             cursor.execute(show_table)
-             print(f"Table creares successfully:\t {cursor.fetchall()}")
+            _SQL = "insert into log (`phrase`, `letters`, `ip`, `browser_string`, `result`) VALUES (%s, %s, %s, %s, %s)"
+            cursor.execute(_SQL, ('hitch-hiker', 'xyz' , '127.0.0.1', 'Firefox','set()'))
+            connection.commit()
+            _SQL = """SELECT * FROM LOG"""
+            cursor.execute(_SQL)
+            for row in cursor.fetchall():
+                print(row)
+
+            #   (%(phrase)s, %(letters)s, %(ip)s, %(Brouser)s, %(result)s)
+            # ('hitch-hiker','aeiou','127.0.0.1','Firefox',"{'e','i'}")
+            #  _SQL = """describe log"""
+            #  cursor.execute(_SQL)
+            #  pp = pprint.PrettyPrinter(width=41, compact=True)
+            #  print("Info from Date Base:\t")
+            #  for row in cursor.fetchall():
+            #      print(row)
+            # pprint.pprint(cursor.fetchall())
 
     finally:
         connection.close()
--- vsearch4web.py
+++ vsearch4web.py
@@ -1,60 +1,114 @@
-from flask import Flask, render_template, request #requst and render html pages
-from vsearch import search_4_letters #my func - using my func in project 
-from flask import escape #Экранирование что бы браузер не воспринимал текст как теги например "< or >"
+from flask import Flask, render_template, request  # requst and render html pages
+from vsearch import search_4_letters  # my func - using my func in project
+# Экранирование что бы браузер не воспринимал текст как теги например "< or >"
+from flask import escape
+import pymysql
+from db.config import host, user, password, db_name
+from ua_parser import user_agent_parser
 
 app = Flask(__name__)
 
-def log_request(req, res: str) -> None:
+def log_request_text(req, res: str) -> None:
     with open('vsearch.log', 'a') as log:
-         print(req.form, req.remote_addr, req.user_agent, res, file=log, sep = "|")
+         req_user_browser = user_agent_parser.Parse(req.user_agent.string)['user_agent']['family']
+         print(req.form, req.remote_addr, req_user_browser, res, file=log, sep = "|")
 
-@app.route('/search4', methods = ['POST'])
+def log_request(req, res: str) -> None:  # записать в лог (работает с бд)
+
+    try:
+        # Подключаемся к Mysql с помощью драйвера
+        connection = pymysql.connect(
+        host=host,
+        port=3306,
+        user=user,
+        password=password,
+        database=db_name,
+        # это нужно для того, чтобы получить результат в виде словаря, где ключами будут названия колонок.
+        cursorclass=pymysql.cursors.DictCursor)
+        print("Successfully connected")
+        print('"#' * 20)
+
+
+        try:
+            with connection.cursor() as cursor: #автоматически закрое соединение с cursor
+                _SQL = "insert into log (`phrase`, `letters`, `ip`, `browser_string`, `result`) VALUES (%s, %s, %s, %s, %s)"
+                req_user_browser = user_agent_parser.Parse(req.user_agent.string)['user_agent']['family']
+                cursor.execute(_SQL, (req.form['phrase'],
+                                      req.form['letters'],
+                                      req.remote_addr,
+                                      req.user_agent.string,
+                                      req_user_browser,))
+                connection.commit()  # принудительное сохранение в БД
+
+        except Exception as ex:  # Ловим ошибку при покдлючении к бд
+            print("Error cursor - execute")
+            print(ex)
+
+        finally:
+            connection.close()  # закрываем соединение с бд
+
+    except Exception as ex:  # Ловим ошибку при покдлючении к бд
+        print("Connection refused")
+        print(ex)
+
+    # Старая запись в лог
+    # with open('vsearch.log', 'a') as log:
+    #      print(req.form, req.remote_addr, req.user_agent, res, file=log, sep = "|")
+
+
+@app.route('/search4', methods=['POST'])
 def do_search() -> str:
     """Функция для вывода и постановки в шаблон наших значений в html шаблоне
     с помощью junja2
     """
-    log_request(request, str(search_4_letters(request.form['phrase'], request.form['letters']))) #записать в лог (делает файл в корне)
+    log_request_text(request, str(search_4_letters(
+        request.form['phrase'], request.form['letters']))) # запись в лог
+    
+    log_request(request, str(search_4_letters(
+        request.form['phrase'], request.form['letters'])))  # вызываем функция которая будет записывать в бд
+
     return render_template('result.html',
-                           the_title = 'Here are you result:',
-                           the_phrase = request.form['phrase'],
-                           the_letters = request.form['letters'],
-                           the_result = str(search_4_letters(request.form['phrase']
-                                                             ,request.form['letters'])), )  #рендеринг нашей html страницы
+                           the_title='Here are you result:',
+                           the_phrase=request.form['phrase'],
+                           the_letters=request.form['letters'],
+                           the_result=str(search_4_letters(request.form['phrase'], request.form['letters'])), )
+
+    # return render_template('result.html',
+    #                        the_title = 'Here are you result:',
+    #                        the_phrase = request.form['phrase'],
+    #                        the_letters = request.form['letters'],
+    #                        the_result = str(search_4_letters(request.form['phrase']
+    #                                                          ,request.form['letters'])), )  #рендеринг нашей html страницы
+
 
 @app.route('/')
 @app.route('/entry')
 def entry_page() -> 'str':
     return render_template('entry.html',
-                           the_title = 'Welcome to search4letterson the web')
+                           the_title='Welcome to search4letterson the web')
 
 
 @app.route('/viewlog')
 def view_the_log() -> 'str':
     contents = []
     with open('vsearch.log') as log:
-         for line in log:
+        for line in log:
             contents.append([])
-            for item in line.split('|'): #сделано под escape (чтбы у нас был список из escape)
-                contents[-1].append(escape(item)) 
-    titles = ('Form Data','Remote_addr','User_agent','Result',)
+            # сделано под escape (чтбы у нас был список из escape)
+            for item in line.split('|'):
+                contents[-1].append(escape(item))
+    titles = ('Form Data', 'Remote_addr', 'User_agent', 'Result',)
     return render_template('viewlog.html',
-                            the_title='View_Log',
-                            the_row_titles=titles,
-                            the_data=contents,)
-
-
+                           the_title='View_Log',
+                           the_row_titles=titles,
+                           the_data=contents,)
 
     # contents = []
     # with open('vsearch.log') as log:
     #     for chose in log:
-    #         contents.append(escape(chose.split('|'))) 
+    #         contents.append(escape(chose.split('|')))
     # return str(contents)
 
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-
-
-   
