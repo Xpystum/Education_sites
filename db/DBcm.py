ba3417e67d3ec4d1ed85a3a6b3bc1930149c875a
Xpystum <qjq3@mail.ru>
2023-03-21 16:19:29 +0300

    бд + class + with
    работа с бд + с абстракцией и менеджером контекста with

---
+++ db/DBcm.py
@@ -0,0 +1,32 @@
+import pymysql
+
+class DbConfing: 
+    def __init__(self,host:str , user: str , port:int ,password: str , db_name: str) -> None:
+         self.host = host
+         self.user = user
+         self.port = port
+         self.password = password
+         self.database = db_name
+
+class UseDatabase:
+    def __init__(self, DbConfing) -> None:
+        self.host = DbConfing.host
+        self.user = DbConfing.user
+        self.port = DbConfing.port
+        self.password = DbConfing.password
+        self.database = DbConfing.database
+    def __enter__(self) -> 'cursor':
+        self.connection = pymysql.connect(
+                            host = self.host,
+                            port = self.port,
+                            user = self.user,
+                            password =  self.password,
+                            database = self.database,
+                            cursorclass = pymysql.cursors.DictCursor,)# это нужно для того, чтобы получить результат в виде словаря, где ключами будут названия колонок.
+        self.cursor = self.connection.cursor()
+        return self.cursor
+    def __exit__(self, exc_type, exc_value,exc_trace) -> None:
+        self.connection.commit()
+        self.cursor.close()
+        self.connection.close()
+        
--- vsearch4web.py
+++ vsearch4web.py
@@ -2,9 +2,10 @@ from flask import Flask, render_template, request  # requst and render html page
 from vsearch import search_4_letters  # my func - using my func in project
 # Экранирование что бы браузер не воспринимал текст как теги например "< or >"
 from flask import escape
-import pymysql
-from db.config import host, user, password, db_name
 from ua_parser import user_agent_parser
+from db.DBcm import DbConfing, UseDatabase
+
+
 
 app = Flask(__name__)
 
@@ -14,50 +15,32 @@ def log_request_text(req, res: str) -> None:
          req_user_browser = user_agent_parser.Parse(req.user_agent.string)['user_agent']['family']
          print(req.form, req.remote_addr, req_user_browser, res, file=log, sep = "|")
 
-def log_request(req, res: str) -> None:  # записать в лог (работает с бд)
+def log_request(req, res: str) -> None:  # Работа с Базой данных
+
+    app.config['DbConfing'] = DbConfing("127.0.0.1", "root", 3306 ,"1911" , "vsearchlogdb") # поместили данные о подключении в объект
+    #Для гибкости лучше использовать паттерн абстрактная фабрика (Что бы мы могли менять субд не переписывая наши объекты)
+    
 
     try:
-        # Подключаемся к Mysql с помощью драйвера
-        connection = pymysql.connect(
-        host = host,
-        port = 3306,
-        user = user,
-        password = password,
-        database = db_name,
-        # это нужно для того, чтобы получить результат в виде словаря, где ключами будут названия колонок.
-        cursorclass = pymysql.cursors.DictCursor)
-        print("Successfully connected")
-        print('"#' * 20)
+           
+        with UseDatabase(app.config['DbConfing']) as cursor: #Организовали наш класс с менеджером контекста к бд
+            print("Successfully connected")
+            print('"#' * 20)
+            _SQL = "insert into log (`phrase`, `letters`, `ip`, `browser_string`, `result`) VALUES (%s, %s, %s, %s, %s)"
+            req_user_browser = user_agent_parser.Parse(req.user_agent.string)['user_agent']['family']
+            cursor.execute(_SQL, (req.form['phrase'],
+                                        req.form['letters'],
+                                        req.remote_addr,
+                                        req.user_agent.string,
+                                        req_user_browser,))
+            print("Commit - is add in Date Base")
 
 
-    #Сделать шаблонную функцию в функции
-
-        try:
-            with connection.cursor() as cursor: #автоматически закрое соединение с cursor
-                _SQL = "insert into log (`phrase`, `letters`, `ip`, `browser_string`, `result`) VALUES (%s, %s, %s, %s, %s)"
-                print(type(req_user_browser = user_agent_parser.Parse(req.user_agent.string)['user_agent']['family']))
-                cursor.execute(_SQL, (req.form['phrase'],
-                                      req.form['letters'],
-                                      req.remote_addr,
-                                      req.user_agent.string,
-                                      req_user_browser,))
-                connection.commit()  # принудительное сохранение в БД
-
-        except Exception as ex:  # Ловим ошибку при покдлючении к бд
-            print("Error cursor - execute")
-            print(ex)
-
-        finally:
-            connection.close()  # закрываем соединение с бд
-
-    except Exception as ex:  # Ловим ошибку при покдлючении к бд
-        print("Connection refused")
+    except Exception as ex:  # Ловим ошибку при покдлючении к бд или при работе
+        print("Connection refused Date Base or inside the context manager `with` ")
         print(ex)
 
-    # Старая запись в лог
-    # with open('vsearch.log', 'a') as log:
-    #      print(req.form, req.remote_addr, req.user_agent, res, file=log, sep = "|")
-#Конец блоков Функций
+
 
 @app.route('/search4', methods=['POST'])
 def do_search() -> str:
@@ -76,13 +59,7 @@ def do_search() -> str:
                            the_letters=request.form['letters'],
                            the_result=str(search_4_letters(request.form['phrase'], request.form['letters'])), )
 
-    # return render_template('result.html',
-    #                        the_title = 'Here are you result:',
-    #                        the_phrase = request.form['phrase'],
-    #                        the_letters = request.form['letters'],
-    #                        the_result = str(search_4_letters(request.form['phrase']
-    #                                                          ,request.form['letters'])), )  #рендеринг нашей html страницы
-
+  
 
 @app.route('/')
 @app.route('/entry')
@@ -93,25 +70,26 @@ def entry_page() -> 'str':
 
 @app.route('/viewlog')
 def view_the_log() -> 'str':
-    contents = []
-    with open('vsearch.log') as log:
-        for line in log:
-            contents.append([])
-            # сделано под escape (чтбы у нас был список из escape)
-            for item in line.split('|'):
-                contents[-1].append(escape(item))
-    titles = ('Form Data', 'Remote_addr', 'User_agent', 'Result',)
+
+    with UseDatabase(app.config['DbConfing']) as cursor:
+        _SQL = "select phrase, letters, ip, browser_string, result from log"
+
+                   
+        cursor.execute(_SQL)
+        result = cursor.fetchall() #возвращает данные в зависимости от запроса (в данном примере словарь)
+
+        lits_result = [] 
+        for a in result:
+            lits_result.append(escape(list(a.values()))) #нужно для того т.к fetchall() - теперь возвращает словарь
+            
+
+    titles = ('Phrase','Letters', 'Remote_addr', 'User_agent', 'Result',)
     return render_template('viewlog.html',
                            the_title='View_Log',
                            the_row_titles=titles,
-                           the_data=contents,)
+                           the_data = lits_result,)
 
-    # contents = []
-    # with open('vsearch.log') as log:
-    #     for chose in log:
-    #         contents.append(escape(chose.split('|')))
-    # return str(contents)
-
+         
 
 if __name__ == '__main__':
     app.run(debug=True)
